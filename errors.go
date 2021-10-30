834fc9c0ffb8b5bd126569f80a027532ad02b448
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2021-10-30 22:44:12 +1000

    errors in a separate file

---
+++ errors.go
@@ -0,0 +1,31 @@
+package tbcomctl
+
+import "errors"
+
+// ErrType is the type of error returned by the callback functions.
+type ErrType int
+
+const (
+	TErrNoChange ErrType = iota // there has been no change to the selection
+	TErrRetry                   // error of this type will ask user to retry the selection or input
+	TInputError                 // tell user that there was an input error (user-error)
+)
+
+// Error is the type of error returned by the input-processing callback functions
+type Error struct {
+	Alert bool
+	Msg   string
+	Type  ErrType
+}
+
+func (e *Error) Error() string { return e.Msg }
+
+var (
+	// ErrRetry should be returned by CallbackFunc if the retry should be performed.
+	ErrRetry = &Error{Type: TErrRetry, Msg: "retry", Alert: true}
+	// ErrNoChange should be returned if the user picked the same value as before, and no update needed.
+	ErrNoChange = &Error{Type: TErrNoChange, Msg: "no change"}
+)
+
+// BackPressed is a special type of error indicating that callback handler should call the previous handler.
+var BackPressed = errors.New("back")
